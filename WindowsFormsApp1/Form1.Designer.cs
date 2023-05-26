b4d6616a7ae4eb970e1b5e1fddcc5acb6547a961
Kacper <Kacper@DESKTOP-2BNHDKK>
2023-05-26 22:19:34 +0200

    Dodaj pliki projektów.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(485, 595);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "file");
+            this.imageList1.Images.SetKeyName(1, "dir");
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(767, 32);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(485, 595);
+            this.listView2.SmallImageList = this.imageList1;
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(764, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 663);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int minimumColumnWidth = 40;
+        private double ratio1;
+        private double ratio2;
+        private string path1;
+        private string path2;
+
+        
+            
+        public Form1()
+        {
+            InitializeComponent();
+            MinimumSize = new Size(800, 600);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //autosize
+            int listSize = (ClientSize.Width - 74) / 2;
+            listView1.Width = listSize;
+            listView2.Width = listSize;
+            listView2.Left = listView1.Right + 50;
+            label2.Left = listView2.Left;
+            label1.Left = listView1.Left;
+
+            SizeChanged += Form1_SizeChanged;
+
+            //left list
+            int list0Width = listView1.ClientSize.Width;
+            listView1.Columns[0].Width = list0Width * 3 / 4;
+            listView1.Columns[1].Width = list0Width * 1 / 4;
+            ratio1 = (double)listView1.Columns[0].Width / listView1.Width;
+            label1.Left = listView1.Left; 
+            ShowDIR(@"C:\", listView1);
+            path1 = @"C:\";
+
+            listView1.ColumnWidthChanging += ListView_ColumnWidthChanging;
+            listView1.ColumnWidthChanged += ListView1_ColumnWidthChanged;
+            listView1.ItemActivate += ListView_ItemActivate;
+
+
+            //right list
+            int list1Width = listView2.ClientSize.Width;
+            listView2.Columns[0].Width = list1Width * 3 / 4;
+            listView2.Columns[1].Width = list1Width * 1 / 4;
+            ratio2 = (double)listView2.Columns[0].Width / listView2.Width;
+            label2.Left = listView2.Left;
+            ShowDIR(@"D:\", listView2);
+            path2 = @"D:\";
+
+            listView2.ItemActivate += ListView_ItemActivate;
+            listView2.ColumnWidthChanging += ListView_ColumnWidthChanging;
+            listView2.ColumnWidthChanged += ListView2_ColumnWidthChanged;
+            
+        }
+        
+
+        private void ListView_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            ListView listView = (ListView)sender;
+            if (e.ColumnIndex == 1)
+            {
+                e.Cancel = true;
+                e.NewWidth = listView.Columns[1].Width;
+            }
+            if (e.NewWidth < minimumColumnWidth)
+            {
+                e.Cancel = true;
+                e.NewWidth = minimumColumnWidth;
+            }
+            if (e.NewWidth > listView.ClientSize.Width - minimumColumnWidth)
+            {
+                e.Cancel = true;
+                e.NewWidth = listView.ClientSize.Width - minimumColumnWidth;
+            }
+        }
+        private void ListView1_ColumnWidthChanged(object sender, ColumnWidthChangedEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                listView1.Columns[1].Width = listView1.ClientSize.Width - listView1.Columns[0].Width;
+                ratio1 = (double)listView1.Columns[0].Width / listView1.Width;
+            }
+        }
+        
+        private void ListView2_ColumnWidthChanged(object sender, ColumnWidthChangedEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                listView2.Columns[1].Width = listView2.ClientSize.Width - listView2.Columns[0].Width;
+                ratio2 = (double)listView2.Columns[0].Width / listView2.Width;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int listSize = (ClientSize.Width - 74) / 2;
+            listView1.Width = listSize;
+            listView2.Width = listSize;
+            listView2.Left = listView1.Right + 50;
+            label2.Left = listView2.Left;
+            
+
+            if ((int)(listSize * ratio1) >= minimumColumnWidth)
+            {
+                listView1.Columns[0].Width = (int)(listSize * ratio1);
+            }
+            else
+            {
+                listView1.Columns[0].Width = minimumColumnWidth;
+            }
+            listView1.Columns[1].Width = listSize - listView1.Columns[0].Width - 5;
+        
+            if ((int)(listSize * ratio2) >= minimumColumnWidth)
+            {
+                listView2.Columns[0].Width = (int)(listSize * ratio2);
+            }
+            else
+            {
+                listView2.Columns[0].Width = minimumColumnWidth;
+            }
+            listView2.Columns[1].Width = listSize - listView2.Columns[0].Width - 5;
+
+           
+        }
+
+        
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowDIR(string directoryPath, ListView listView)
+        {
+            listView.Items.Clear();
+            if (listView == listView1)
+            {
+                
+                label1.Text = "Path: " + directoryPath;
+                path1 = directoryPath;
+            }
+            else if (listView == listView2)
+            {
+                label2.Text = "Path: " + directoryPath;
+                path2 = directoryPath;
+            }
+
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+
+            if (directoryInfo.Parent != null)
+            {
+                ListViewItem parentItem = new ListViewItem("..");
+                parentItem.SubItems.Add("");
+                parentItem.ImageKey = "dir";
+                listView.Items.Add(parentItem);
+            }
+
+
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                ListViewItem item = new ListViewItem(dir.Name);
+                item.ImageKey = "dir";
+                item.SubItems.Add(dir.LastWriteTime.ToString("yyyy-MM-dd HH:mm"));
+                listView.Items.Add(item);
+            }
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(file.Name);
+                item.ImageKey = "file";
+                item.SubItems.Add(file.LastWriteTime.ToString("yyyy-MM-dd HH:mm"));
+                listView.Items.Add(item);
+            }
+        }
+        private void ListView_ItemActivate(object sender, EventArgs e)
+        {
+            ListView listView = (ListView)sender;
+            ListViewItem item = listView.SelectedItems[0];
+            string itemName = item.Text;
+            string itemPath;
+            if (listView == listView1)
+            {
+                itemPath = Path.Combine(path1, itemName);
+            }
+            else
+            {
+                itemPath = Path.Combine(path2, itemName);
+            }
+
+            if (Directory.Exists(itemPath))
+            {
+                
+                try
+                {
+                    if (itemName == "..")
+                    {
+                        if(listView == listView1)
+                        {
+                            itemPath = Directory.GetParent(path1).FullName;
+                        }
+                        else
+                        {
+                            itemPath = Directory.GetParent(path2).FullName;
+                        }
+                    }
+                    ShowDIR(itemPath, listView);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("ERROR: " + error.Message);
+                    ShowDIR(Directory.GetParent(itemPath).FullName, listView);
+                }
+
+            }
+            else if (File.Exists(itemPath))
+            {
+                Process.Start(itemPath);
+            }
+        }
+    }
+}
